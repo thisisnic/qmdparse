b4798c3badaf1b129ee23574140f218028598bea
Nic Crane <thisisnic@gmail.com>
2023-12-28 08:22:58 +0000

    Allow to work with objects like a list

--- R/annotate.R
+++ R/annotate.R
@@ -2,35 +2,34 @@
 #'
 #' @param file_contents vector of file contents
 #' @return vector of annotations ("code", "yaml", or "text")
-annotate_top_level <- function(file_contents){
+annotate_top_level <- function(file_contents) {
   file_annotations <- vector(mode = "character", length = length(file_contents))
 
   code_context <- FALSE
   yaml_context <- FALSE
 
-  for(i in seq_along(file_contents)){
-
+  for (i in seq_along(file_contents)) {
     line <- file_contents[i]
 
-    if(code_context){
+    if (code_context) {
       file_annotations[i] <- "code"
-      if(grepl("^```", line)){
+      if (grepl("^```", line)) {
         code_context <- FALSE
       }
       # non-code context
-    } else if(yaml_context) {
+    } else if (yaml_context) {
       # we are still in a yaml context
       file_annotations[i] <- "yaml"
       # if we find a "---", then we end the yaml context
-      if(grepl("^---", line)){
+      if (grepl("^---", line)) {
         yaml_context <- FALSE
       }
       # if we are not in a yaml context but detect the start of one
-    } else if(grepl("^---", line)){
+    } else if (grepl("^---", line)) {
       yaml_context <- TRUE
       file_annotations[i] <- "yaml"
       # if we are not in a code context but detect the start of one
-    } else if(grepl("^```", line)){
+    } else if (grepl("^```", line)) {
       code_context <- TRUE
       file_annotations[i] <- "code"
     } else {
@@ -44,40 +43,54 @@ annotate_top_level <- function(file_contents){
 #'
 #' @param annotations vector of annotations
 #' @return tibble with start, end, and type columns
-extract_children <- function(annotations, file_contents){
-
+extract_children <- function(annotations, file_contents, start = 1, end = length(file_contents)) {
   run_lengths <- rle(annotations)
-  end <- cumsum(run_lengths$lengths)
+  end_indices <- cumsum(run_lengths$lengths) + (start - 1)
 
   # If the last index isn't the end of the file we need to go up to it
-  if(end[length(end)] < length(annotations)){
-    end <- c(end, length(annotations))
+  if (end_indices[length(end_indices)] < end) {
+    end_indices <- c(end_indices, end)
   }
 
-  start <- c(1, lag(end) + 1)
-  start <- start[-length(start)]
-  types <- annotations[start]
-  lapply(seq_along(types), function(i){
-    new_section(types[i], start[i], end[i], contents = file_contents[start[i]:end[i]])
+  start_indices <- c(start, lag(end_indices) + 1)
+  start_indices <- start_indices[-length(start_indices)]
+  types <- annotations[start_indices - (start - 1)]
+  lapply(seq_along(types), function(i) {
+    new_section(
+      types[i],
+      start_indices[i],
+      end_indices[i],
+      contents = file_contents[start_indices[i]:end_indices[i]]
+    )
   })
 }
 
-annotate_block <- function(){
+annotate_block <- function(contents) {
+  block_annotations <- vector(mode = "character", length = length(contents))
 
+  for (i in seq_along(contents)) {
+    line <- contents[i]
+    if (grepl("^#", line)) {
+      block_annotations[i] <- "heading"
+    } else {
+      block_annotations[i] <- "paragraph"
+    }
+  }
+  block_annotations
 }
 
 
-annotate_text <- function(file_contents, sections){
+annotate_text <- function(file_contents, sections) {
   text_sections <- sections[sections$type == "text", ]
 
   text_annotations <- list()
 
-  map2(text_sections$start, text_sections$end, function(start, end){
-    for(i in start:end){
+  map2(text_sections$start, text_sections$end, function(start, end) {
+    for (i in start:end) {
       line <- file_contents[i]
-      if(grepl("^#", line)){
+      if (grepl("^#", line)) {
         text_annotations[length(text_annotations) + 1] <- list(line = i, type = "header")
-      } else if(line == ""){
+      } else if (line == "") {
         text_annotations[length(text_annotations) + 1] <- list(line = i, type = "blank")
       } else {
         text_annotations[length(text_annotations) + 1] <- list(line = i, type = "text")
--- R/qmdparse_class.R
+++ R/qmdparse_class.R
@@ -1,4 +1,4 @@
-qmdparse <- function(path){
+qmdparse <- function(path) {
   qmdparsedoc <- qmdparse_doc$new(path)
   qmdparsedoc
 }
@@ -6,14 +6,22 @@ qmdparse <- function(path){
 qmdparse_obj <- R6Class(
   "qmdparse_obj",
   public = list(
-    get_children = function(){
+    get_children = function() {
       private$children
+    },
+    get_child = function(i) {
+      private$children[[i]]
     }
   ),
   private = list(
     children = list()
   )
 )
+
+`[[.qmdparse_obj` <- function(x, i) {
+  x$get_child(i)
+}
+
 
 qmdparse_doc <- R6Class(
   "qmdparse_doc",
@@ -22,30 +30,29 @@ qmdparse_doc <- R6Class(
     initialize = function(path) {
       private$path <- path
       private$type <- get_doc_type(path)
-      private$doc_contents <- readLines(path)
+      private$contents <- readLines(path)
       private$set_children()
-
     },
-    get_doc_contents = function(){
-      private$doc_contents
+    get_contents = function() {
+      private$contents
     }
   ),
   private = list(
     path = NULL,
-    doc_contents = NULL,
+    contents = NULL,
     type = NULL,
-    set_children = function(){
-      tla = annotate_top_level(private$doc_contents)
-      private$children = extract_children(tla, private$doc_contents)
+    set_children = function() {
+      tla <- annotate_top_level(private$contents)
+      private$children <- extract_children(tla, private$contents)
     }
   )
 )
 
 
-get_doc_type <- function(path){
-  if(grepl("\\.rmd$", path, ignore.case = TRUE)){
+get_doc_type <- function(path) {
+  if (grepl("\\.rmd$", path, ignore.case = TRUE)) {
     "rmd"
-  } else if(grepl("\\.qmd$", path, ignore.case = TRUE)){
+  } else if (grepl("\\.qmd$", path, ignore.case = TRUE)) {
     "qmd"
   } else {
     rlang::abort("File type must be .Rmd or .qmd")
@@ -57,10 +64,10 @@ qmdparse_block <- R6Class(
   "qmdparse_block",
   inherit = qmdparse_obj,
   public = list(
-    initialize = function(start, end, contents){
-      private$start = start
-      private$end = end
-      private$contents = contents
+    initialize = function(start, end, contents) {
+      private$start <- start
+      private$end <- end
+      private$contents <- contents
       private$set_children()
     }
   ),
@@ -68,31 +75,55 @@ qmdparse_block <- R6Class(
     start = NULL,
     end = NULL,
     contents = NULL,
-    set_children = function(){
-      block = annotate_block(private$contents, private$start, private$end)
-      private$children = extract_children(block, private$contents)
+    set_children = function() {
+      block <- annotate_block(private$contents)
+      private$children <- extract_children(block, private$contents, private$start, private$end)
     }
   )
 )
 
 qmdparse_heading <- R6Class(
   "qmdparse_heading",
-  inherit = qmdparse_obj
+  inherit = qmdparse_obj,
+  public = list(
+    initialize = function(start, end, contents) {
+      private$start <- start
+      private$end <- end
+      private$contents <- contents
+    }
+  ),
+  private = list(
+    start = NULL,
+    end = NULL,
+    contents = NULL
+  )
 )
 
 qmdparse_paragraph <- R6Class(
   "qmdparse_paragraph",
-  inherit = qmdparse_obj
+  inherit = qmdparse_obj,
+  public = list(
+    initialize = function(start, end, contents) {
+      private$start <- start
+      private$end <- end
+      private$contents <- contents
+    }
+  ),
+  private = list(
+    start = NULL,
+    end = NULL,
+    contents = NULL
+  )
 )
 
 qmdparse_code <- R6Class(
   "qmdparse_code",
   inherit = qmdparse_obj,
   public = list(
-    initialize = function(start, end, contents){
-      private$start = start
-      private$end = end
-      private$contents = contents
+    initialize = function(start, end, contents) {
+      private$start <- start
+      private$end <- end
+      private$contents <- contents
     }
   ),
   private = list(
@@ -106,10 +137,10 @@ qmdparse_yaml <- R6Class(
   "qmdparse_yaml",
   inherit = qmdparse_obj,
   public = list(
-    initialize = function(start, end, contents){
-      private$start = start
-      private$end = end
-      private$contents = contents
+    initialize = function(start, end, contents) {
+      private$start <- start
+      private$end <- end
+      private$contents <- contents
     }
   ),
   private = list(
@@ -119,10 +150,12 @@ qmdparse_yaml <- R6Class(
   )
 )
 
-new_section <- function(type, start, end, contents){
+new_section <- function(type, start, end, contents) {
   switch(type,
-         "code" = qmdparse_code$new(start, end, contents),
-         "yaml" = qmdparse_yaml$new(start, end, contents),
-         "text" = qmdparse_block$new(start, end, contents)
+    "code" = qmdparse_code$new(start, end, contents),
+    "yaml" = qmdparse_yaml$new(start, end, contents),
+    "text" = qmdparse_block$new(start, end, contents),
+    "heading" = qmdparse_heading$new(start, end, contents),
+    "paragraph" = qmdparse_paragraph$new(start, end, contents)
   )
 }
