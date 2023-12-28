8fb2cd3d19e0043ff0e45bd2274fe2283ed7b043
Nic Crane <thisisnic@gmail.com>
2023-12-28 08:54:26 +0000

    Remove unused functions

--- R/parse_code.R
+++ R/parse_code.R
@@ -1,23 +1,7 @@
-#' Create AST for code block
-#'
-parse_code <- function(){
-
-}
-
-#' Create AST for non-code block
-#'
-parse_non_code <- function(){
-
-}
-
 #' @export
 parse_qmd <- function(file){
 
   parsed_doc <- qmdparse_doc$new(file)
-
-  # TODO: parse code blocks
-  # TODO: parse non-code blocks
-
   parsed_doc
 }
 
