dc578277272927b3f4c2fa2b18bbfcfe4b24f336
Nic Crane <thisisnic@gmail.com>
2024-01-01 10:02:26 +0000

    Comment out qmd_map for now

--- R/qmd_map.R
+++ R/qmd_map.R
@@ -1,21 +1,21 @@
-#' Map a function to particular sections of a qmd doc
+#' #' Map a function to particular sections of a qmd doc
+#' #'
+#' #' @param .path Path to qmd doc
+#' #' @param .f Function to map
+#' #' @param ... Additional arguments to pass to function
+#' #' @param .type Type of section to map to
+#' #'
+#' #' @examples
+#' #' qmd_map("qmds/simple_doc.qmd", stringi::str_count_words, .type = "text")
+#' qmd_map <- function(.path, .f, ..., .type = "text"){
+#'   parsed_doc <- parse_qmd(path)
 #'
-#' @param .path Path to qmd doc
-#' @param .f Function to map
-#' @param ... Additional arguments to pass to function
-#' @param .type Type of section to map to
+#'   targets <- list()
+#'   # extract the items of the correct type
 #'
-#' @examples
-#' qmd_map("tests/testthat/example_doc.qmd", stringi::str_count_words, .type = "text")
-qmd_map <- function(.path, .f, ..., .type = "text"){
-  parsed_doc <- parse_qmd(path)
-
-  targets <- list()
-  # extract the items of the correct type
-
-
-  purrr::map(targets, .f, ...)
-
-}
-
-#TODO: decide if we want this or just level stuff actually
+#'
+#'   purrr::map(targets, .f, ...)
+#'
+#' }
+#'
+#' #TODO: decide if we want this or just level stuff actually
