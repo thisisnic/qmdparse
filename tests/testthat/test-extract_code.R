2ad29739a575e155c0dcea083ec542e50714bc6e
Nic Crane <thisisnic@gmail.com>
2024-02-27 18:12:23 -0500

    Fix failing test

--- tests/testthat/test-extract_code.R
+++ tests/testthat/test-extract_code.R
@@ -5,6 +5,6 @@ test_that("extract_code extracts all code from a qmd", {
   expect_length(out_code, 1L)
   expect_true(inherits(out_code[[1]], "qmdparse_code"))
 
-  expect_error(extract_code("qmds/simple_doc.qmd"), "`x` must be a qmdparse_doc, not a character")
+  expect_error(extract_code("qmds/simple_doc.qmd"), "`x` must be a qmdparse_obj object, not a character")
 
 })
