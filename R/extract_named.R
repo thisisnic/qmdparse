036629c990361cf73f985a3e426b0200e923c4a6
Nic Crane <thisisnic@gmail.com>
2024-02-27 18:57:58 -0500

    Create extract_named function

---
+++ R/extract_named.R
@@ -0,0 +1,15 @@
+#' Extract all elements from a parsed Quarto markdown doc with a matching name
+#'
+#' @param x qmdparse_object object
+#' @param name name of the element to extract
+#' @export
+extract_named <- function(x, name){
+
+  if(!is_qmd_object(x)){
+    rlang::abort(paste0("`x` must be a qmdparse_obj object, not a ", class(x)[[1]]))
+  }
+
+  flat_x <- qmd_doc_flatten(x, keep_parents = TRUE)
+
+  flat_x[[which(names(flat_x) == name)]]
+}
